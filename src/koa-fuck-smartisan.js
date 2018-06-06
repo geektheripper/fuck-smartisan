29ee8b590da12eb6c99f6c9cc032f6ffba335576
jeffjing <zgayjjf@qq.com>
2018-06-06 11:35:07 +0800

    update:增加默认信息

--- src/koa-fuck-smartisan.js
+++ src/koa-fuck-smartisan.js
@@ -1,6 +1,14 @@
 const isSmartisanOs = require('./is-smartisan-os.js')
 
-function fuckSmartisanOs(message) {
+const defaultMessage = '因开开发者能力有限，本页面不对锤子手机进行兼容，理解万岁。'
+
+/**
+ * res page and with a error alert
+ * 
+ * @export
+ * @param {string|Array} [message='因开开发者能力有限，本页面不对锤子手机进行兼容，理解万岁。'] 
+ */
+module.exports = function fuckSmartisanOs(message = defaultMessage) {
   return async function (ctx, next) {
     if (isSmartisanOs(ctx.request.headers['user-agent'])) {
       ctx.body = message
